9fbd5552fbce94db24423b4a3b143e17519a417d
Florlucana <lucanahuamaniflormilagros@gmail.com>
2024-05-27 12:53:04 -0500

    actualizado

--- tipos_datos_estructurados/homework.py
+++ tipos_datos_estructurados/homework.py
@@ -1,28 +1,43 @@
-# Crear una lista de 5 alumnos, cada alumno almacenaremos el nombre,apellido y edad.
-# Insertar al final de la lista las datos de Anthony.
-# Eliminar la lista si existe los datos de Abel.
-# Buscar y mostrar el alumno que se encuentre en la posición 4de lalista. 
-lista_alumnos=[
+# Crear una lista de alumnos
+lista_alumnos = [
     {
-        "Nombre"= "Flor",
-        "Apellido"= "Lucana",
-        "Edad"= 18
-
+    "nombre": "Ruth",
+    "apellido": "Castillo",
+    "edad": 18
     },{
-        "Nombre"="Rocío",
-        "Apellido"="Lobo"
-        "Edad"=26
+    "nombre": "Flor", 
+    "apellido": "Lucana", 
+    "edad": 18
     },{
-        "Nombre"="Ruth"
-        "Apellido"="Castillo"
-        "Edad"=18
+    "nombre": "Rocío", 
+    "apellido": "Lobo", 
+    "edad": 20
     },{
-        "Nombre"="Anthony"
-        "Apellido"="Quispe"
-        "Edad"=24
+    "nombre": "Lucy", 
+    "apellido": "Rivera", 
+    "edad": 18
     },{
-        "Nombre"="Lucy"
-        "Apellido""Rivera"
-        "Edad"=18
+    "nombre": "Abel", 
+    "apellido": "Rojas", 
+    "edad": 27
     }
 ]
+
+# Insertar los datos de Anthony al final de la lista
+lista_alumnos.append({"nombre": "Anthony", "apellido": "Smith", "edad": 24})
+
+# Eliminar los datos de Abel si existen en la lista
+lista_alumnos.remove({
+    "nombre":"Abel",
+    "apellido":"Rojas",
+    "edad":27
+})
+print(lista_alumnos)
+
+# Mostrar el alumno en la posición 4 de la lista
+indice=lista_alumnos.index({
+    "nombre":"Pablo",
+    "apellido":"Córdova",
+    "edad":21
+})
+print(lista_alumnos[indice])
