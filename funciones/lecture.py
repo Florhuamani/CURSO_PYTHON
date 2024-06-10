1136f7b4fb10c48dbe1b869f28bfbc91a4d76015
Florlucana <lucanahuamaniflormilagros@gmail.com>
2024-06-10 10:38:20 -0500

    Rellené mis archivos lecture.py y readme.py el día 10 de junio del 2024

--- funciones/lecture.py
+++ funciones/lecture.py
@@ -1,6 +1,6 @@
 # Return devuelve valores que podré hacer uso.
 ## Crear una función que retorne el número 10, y muestre por terminal si es par.
-# Siempre que el valor que retorne mi función se utilize en ,ás sentencias u operaciones 
+# Siempre que el valor que retorne mi función se utilize en más sentencias u operaciones hacer uso de return.
 def diez():
     return 10
 if diez()%2==0:
